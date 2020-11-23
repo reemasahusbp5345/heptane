590c5970006a05416a9578e57f8f995ed60c35d9
Vinay Gupta <62155169+vinaytest75@users.noreply.github.com>
2020-11-23 19:50:06 +0530

    post card updated

--- tumblr-clone/src/App.js
+++ tumblr-clone/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import { Logo } from './Components/Logo'
+// import { Logo } from './Components/Logo'
+import { PostCard } from './Components/PostCard'
 
 function App() {
   return (
     <div className="App">
       Hello world test
-      
+      <PostCard />
     </div>
   );
 }
--- tumblr-clone/src/Components/PostCard.jsx
+++ tumblr-clone/src/Components/PostCard.jsx
@@ -1,98 +1,150 @@
-import React from 'react';
-import styled from 'styled-components';
-import Icon from '@mdi/react'
-import { mdiHeartOutline, mdiReplyOutline, mdiMessageReply, mdiShareVariantOutline    } from '@mdi/js'
+import React from "react";
+import styled from "styled-components";
+import Icon from "@mdi/react";
+import {
+  mdiHeartOutline,
+  mdiReplyOutline,
+  mdiMessageReply,
+  mdiShareVariantOutline,
+  mdiDotsHorizontal,
+} from "@mdi/js";
 
 const CardWrapper = styled.div`
-    border: 1px solid red;
-    height: 600px;
-    width: 500px;
-    margin-left: 30%;
-    display: flex;
-    flex-direction: row;
-`
+  border: 1px solid red;
+  height: 600px;
+  width: 500px;
+  margin-left: 30%;
+  display: flex;
+  flex-direction: row;
+`;
 const AvatarBox = styled.div`
-    border: 1px solid red;
-    margin-top: 10px;
+  border: 1px solid red;
+  margin-top: 10px;
+  height: 70px;
+  width: 70px;
+  margin: 4px;
+
+  & img {
     height: 70px;
     width: 70px;
-    margin: 4px;
-
-    & img {
-        height: 70px;
-        width: 70px;
-    }
-`
+  }
+`;
 
 const PostBox = styled.div`
-    border: 1px solid red;
-    margin-top: 10px;
-    height: 590px;
-    width: 450px;
-    margin: 4px;
-`
+  border: 1px solid red;
+  margin-top: 10px;
+  height: 590px;
+  width: 450px;
+  margin: 4px;
+`;
 const PostBoxHeader = styled.div`
-    border: 1px solid red;
-    position: relative;
-    height: 50px;
-    width: 415px;
-    top: 0px;
+  position: relative;
+  height: 50px;
+  width: 415px;
+  top: 0px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  & div {
+    display: flex;
+    flex-direction: row;
+    padding: 6px;
+    margin: 2px;
+    font-weight: 600;
+
+    & a {
+      margin-top: 7px;
+    }
+  }
+`;
+const PostBoxContent = styled.div`
+  border: 1px solid red;
+  height: 485px;
+  display: flex;
+  flex-direction: column;
+  padding: 5px 5px;
+
+  & div {
     display: flex;
     flex-direction: row;
     justify-content: flex-start;
-    margin-top: 
-`
-const PostBoxContent = styled.div`
-    border: 1px solid red;
-    height: 485px;
-`
+    margin: 5px 7px;
+
+    & a {
+      margin-top: 5px;
+      font-weight: 600;
+    }
+  }
+`;
 
 const PostBoxFooter = styled.div`
-    position: relative;
-    height: 50px;
-    width: 415px;
-    bottom: 0px;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
+  position: relative;
+  height: 50px;
+  width: 415px;
+  bottom: 0px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 
-    & div {
-        margin: 7px;
-    }
+  & div {
+    margin: 7px;
+  }
 
-    & div > * {
-        padding: 5px;
-    }
-`
+  & div > * {
+    padding: 5px;
+  }
+`;
 
 const PostCard = () => {
-    return(
-        <CardWrapper>
-            <AvatarBox>
-                <img src='https://via.placeholder.com/30' alt='avatar' />
-            </AvatarBox>
-            <PostBox>
-                <PostBoxHeader>
-                    <div>username</div>
-                    <a href='https://www.gooogle.com'>Follow</a>
-                </PostBoxHeader>
-                <PostBoxContent>
-                    Content
-                </PostBoxContent>
-                <PostBoxFooter>
-                    <div>
-                        3473 notes
-                    </div>
-                    <div>
-                        <Icon path={ mdiShareVariantOutline  } title='share' size={1} />
-                        <Icon path={ mdiMessageReply  } title='reply' size={1} />
-                        <Icon path={ mdiReplyOutline  } title='reblog' size={1} />                       
-                        <Icon path={ mdiHeartOutline } title='heart' size={1} />
-                    </div>
-                </PostBoxFooter>
-            </PostBox>
-        </CardWrapper>
-    )
-}
+  return (
+    <CardWrapper>
+      {/* Card Header */}
+      <AvatarBox>
+        <img src="https://via.placeholder.com/30" alt="avatar" />
+      </AvatarBox>
 
-export { PostCard }
+      {/* Main Card Content */}
+      <PostBox>
+        <PostBoxHeader>
+          <div>
+            <div>username</div>
+            <a href="https://www.gooogle.com">Follow</a>
+          </div>
+          <div>
+            <Icon path={mdiDotsHorizontal} title="share" size={1} />
+          </div>
+        </PostBoxHeader>
+        <PostBoxContent>
+          <div>
+            <img src="https://via.placeholder.com/30" alt="usernameAvatar" />
+            <div>username</div>
+            <a href="https://www.google.com">Follow</a>
+          </div>
+          <div style={{ width: "40px" }}>
+            <img
+              src="https://via.placeholder.com/400x300"
+              alt="usernameAvatar"
+            />
+          </div>
+          <div>Content</div>
+          <div>Source</div>
+          <div style={{ color: "green", fontWeight: "600" }}>Recommended</div>
+        </PostBoxContent>
+
+        {/* Card Footer */}
+        <PostBoxFooter>
+          <div>3473 notes</div>
+          <div>
+            <Icon path={mdiShareVariantOutline} title="share" size={1} />
+            <Icon path={mdiMessageReply} title="reply" size={1} />
+            <Icon path={mdiReplyOutline} title="reblog" size={1} />
+            <Icon path={mdiHeartOutline} title="heart" size={1} />
+          </div>
+        </PostBoxFooter>
+      </PostBox>
+    </CardWrapper>
+  );
+};
+
+export { PostCard };
