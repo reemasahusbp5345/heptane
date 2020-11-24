a19b9673dd9dcb0246fba3b505293934036d5ebf
Reema <reemasahusbp5345@gmail.com>
2020-11-24 10:05:05 +0530

    dashboard

--- tumblr-clone/src/App.js
+++ tumblr-clone/src/App.js
@@ -1,17 +1,17 @@
 // import { Logo } from './Components/Logo'
-// import { PostCard } from "./Components/PostCard";
-// import { HeadCard } from "./Components/HeadCard";
+import { PostCard } from "./Components/PostCard";
+import { HeadCard } from "./Components/HeadCard";
 // import { RecommendedBlogs } from "./Components/RecommendedBlogs";
 // import { Radar } from "./Components/Radar";
 import { Sponsored } from "./Components/Sponsored";
 import "./App.css";
-// import Routes from './Component/Routes/Routes';
+import Routes from './Component/Routes/Routes';
 
 function App() {
   return (
     <div className="App">
-      <Sponsored />
-      {/* <Routes/> */}
+      
+      <Routes/>
     </div>
   );
 }
